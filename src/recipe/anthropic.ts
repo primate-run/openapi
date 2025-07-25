aa1cd7578da0a8ae14bf3fb7a62bb4b261d18834
terrablue <102580937+terrablue@users.noreply.github.com>
2025-07-25 19:56:03 +0200

    add anthropic

---
+++ src/recipe/anthropic.ts
@@ -0,0 +1,10 @@
+import type Recipe from "#Recipe";
+
+const recipe: Recipe = {
+  description: "Anthropic",
+  aliases: ["claude"],
+  reference: `https://docs.anthropic.com/en/api`,
+  upstream: "https://app.stainless.com/api/spec/documented/anthropic/openapi.documented.yml",
+};
+
+export default recipe;
