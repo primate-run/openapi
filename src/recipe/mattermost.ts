291c172331bbfcc6849fe3f7480619634137289c
terrablue <102580937+terrablue@users.noreply.github.com>
2025-07-27 16:10:20 +0200

    mattermost

---
+++ src/recipe/mattermost.ts
@@ -0,0 +1,9 @@
+import type Recipe from "#Recipe";
+
+const recipe: Recipe = {
+  description: "Discord",
+  reference: "https://developers.mattermost.com/api-documentation",
+  upstream: "https://developers.mattermost.com/mattermost-openapi-v4.yaml",
+};
+
+export default recipe;
