4324e71f94fcf504a3fd7440291ca084a695755b
terrablue <102580937+terrablue@users.noreply.github.com>
2025-07-27 15:47:26 +0200

    add jira and confluence

---
+++ src/recipe/atlassian/confluence.ts
@@ -0,0 +1,9 @@
+import type Recipe from "#Recipe";
+
+const recipe: Recipe = {
+  description: "Confluence Cloud",
+  reference: "https://developer.atlassian.com/cloud/confluence/platform/rest/v2",
+  upstream: "https://developer.atlassian.com/cloud/confluence/openapi-v2.v3.json",
+};
+
+export default recipe;
---
+++ src/recipe/atlassian/jira.ts
@@ -0,0 +1,9 @@
+import type Recipe from "#Recipe";
+
+const recipe: Recipe = {
+  description: "Jira Cloud platform",
+  reference: "https://developer.atlassian.com/cloud/jira/platform/rest/v3",
+  upstream: "https://developer.atlassian.com/cloud/jira/platform/swagger-v3.v3.json",
+};
+
+export default recipe;
