6fef732d3ba50dd4b5074e3b1b8d0a2c966f9d7f
terrablue <102580937+terrablue@users.noreply.github.com>
2025-07-27 15:04:29 +0200

    add crunchbase and wikidata

---
+++ src/recipe/crunchbase.ts
@@ -0,0 +1,9 @@
+import type Recipe from "#Recipe";
+
+const recipe: Recipe = {
+  description: "Crunchbase",
+  reference: "https://data.crunchbase.com/docs",
+  upstream: "https://app.swaggerhub.com/apiproxy/registry/Crunchbase/crunchbase-enterprise_api/1.0.3",
+};
+
+export default recipe;
---
+++ src/recipe/wikidata.ts
@@ -0,0 +1,9 @@
+import type Recipe from "#Recipe";
+
+const recipe: Recipe = {
+  description: "WikiData",
+  reference: "https://www.wikidata.org/wiki/Wikidata:REST_API",
+  upstream: "https://www.wikidata.org/w/rest.php/wikibase/v1/openapi.json",
+};
+
+export default recipe;
